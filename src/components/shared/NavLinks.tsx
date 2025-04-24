c47716365b7839d6482df0418003b9896a5071ab
Mohamed Khassar <mohamedkhassar775@gmail.com>
2025-04-24 19:02:29 +0100

    fix: update NavLinks transition effects for better visibility control

--- src/components/shared/NavLinks.tsx
+++ src/components/shared/NavLinks.tsx
@@ -6,8 +6,8 @@ import NavItem from "./NavItem"
 const NavLinks = ({ className = "" }: { className?: string }) => {
     const isOpen = useAppSelector(state => state.menu.isOpen)
     return (
-        <ul className={cn("flex lg:static fixed transition-[bottom]  duration-600 ease-linear right-0 left-0 lg:items-center gap-8 p-1.5 justify-around max-lg:bg-slate-900 border-t border-slate-700 lg:border-none lg:gap-10 lg:p-0 lg:rounded-none rounded-t-2xl drop-shadow-2xl drop-shadow-blue-200",
-            isOpen ? "bottom-0" : "-bottom-30",
+        <ul className={cn("flex lg:static fixed bottom-0 transition-[visibility_filter_box-shadow_opacity] duration-600 right-0 left-0 lg:items-center gap-8 p-1.5 justify-around max-lg:bg-slate-900 border-t border-slate-700 lg:border-none lg:gap-10 lg:p-0 lg:rounded-none rounded-t-2xl drop-shadow-2xl drop-shadow-blue-200",
+            isOpen ? "opacity-100 visible" : "invisible opacity-0",
             className,
         )}>
             {
